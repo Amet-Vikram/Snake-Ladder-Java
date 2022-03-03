cca748a908ded866eb5bb6be89fac945f57034de
Amet-Vikram <amet1197@gmail.com>
2022-03-03 23:01:53 +0530

    Winning conditions implemented

--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -7,9 +7,8 @@ public class Main {
     static Random random = new Random();
     static Scanner scanner = new Scanner(System.in);
 
-
     static int dice() {
-        System.out.print("Roll the Dice (True/False) ? ");
+        System.out.print("Roll the Dice (True/False) ? "); //Player Event to roll the dice or skip the turn
         boolean playerEvent = scanner.nextBoolean();
         if (playerEvent) {
             System.out.print("You got - ");
@@ -22,16 +21,46 @@ public class Main {
     }
 
     static int GameEvent(){
-        return random.nextInt(3);
+        return random.nextInt(3) ;
     }
 
     public static void main(String[] args) {
-        int rollNumber = dice();
-        System.out.println(rollNumber);
-        switch (GameEvent()) {
-            case 0 -> System.out.println("No Play!");
-            case 1 -> System.out.println("Ladder. Move Ahead " + rollNumber + " steps.");
-            case 2 -> System.out.println("Snakes. Go back " + rollNumber + " steps.");
+        int playerPos = 0;
+//        System.out.println(rollNumber);
+        while (true){
+            int rollNumber = dice();
+            System.out.println(rollNumber);
+            switch (GameEvent()) {
+                case 0 -> System.out.println("No Play!");
+                case 1 -> {
+                    if (playerPos + rollNumber == 100){
+                        System.out.println("You Won");
+                        System.exit(0);
+                    }
+                    else if (playerPos + rollNumber > 100){
+                        System.out.println("Cant move further till you get exactly 100");//Player can't move further
+                    }
+                    else {
+                        System.out.println("LADDERS. Move Ahead " + rollNumber + " steps.");
+                        playerPos += rollNumber;
+                    }
+                }
+                case 2 -> {
+                    if (playerPos <= 0){
+                        System.out.println("SNAKES. You are back to 0. Lol.");//Player can't move below 0.
+                        playerPos = 0;
+                    }
+                    else{
+                        System.out.println("SNAKES. Go back " + rollNumber + " steps.");
+                        playerPos -= rollNumber;
+                    }
+
+
+                }
+            }
+            System.out.println("You are at: " + playerPos);
         }
+
     }
+
 }
